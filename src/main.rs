e897f23677c170146ff992acba7b0c543507d863
Mustafif <mustafif0929@gmail.com>
2022-09-09 19:17:19 -0400

    Added work on projects

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use rocket::{get, launch, routes};
 use std::fs::File;
 use std::io::Write;
 use rocket::fs::NamedFile;
+use rocket::response::Redirect;
+use rocket::uri;
 mod index;
 use index::*;
 
@@ -18,8 +20,22 @@ async fn home() -> Option<NamedFile>{
     NamedFile::open("index.html").await.ok()
 }
 
+#[get("/curr_proj")]
+async fn curr_proj() -> Option<NamedFile>{
+    NamedFile::open("").await.ok()
+}
+
+#[get("/<path>")]
+async fn redirect(path: &str) -> Redirect{
+    match path{
+        "/curr_proj" => Redirect::to(uri!(curr_proj)),
+        _ => Redirect::to(uri!(home))
+    }
+}
+
+
 #[launch]
 fn rocket() -> _{
    rocket::build()
-       .mount("/", routes![home])
+       .mount("/", routes![redirect,home,curr_proj])
 }
--- src/project/mod.rs
+++ src/project/mod.rs
@@ -3,8 +3,17 @@ use std::sync::Mutex;
 use serde::{Deserialize, Serialize};
 use toml::{from_str, to_string_pretty};
 use std::fs::read_to_string;
+use std::path::PathBuf;
 lazy_static!{   
     pub static ref PROJECTS: Mutex<Vec<Project>> = Mutex::new(Vec::new());
+    static ref BASE: &str = r#"<div class="w3-container w3-blue w3-center" style="margin-left: 50px; margin-right: 50px;">
+    <h3>{name}</h3>
+    <h5><a href="{repo}">Repo</a> | <a href="{doc}">Documentation</a> | <a href="{crate}">Crates.io</a></h5>
+    <h5>Current Version: {curr} </h5>
+    <h5>Planned Next Version: {next}</h5>
+    <p class="w3-large">{description}</p>
+</div>
+    "#;
 }
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Project{
@@ -27,7 +36,34 @@ impl Project{
         to_string_pretty(&self).unwrap()
     }
     pub fn to_html(&self) -> String{
-        todo!("Will do once we figure out template")
+        let mut base = BASE.to_string();
+        base = base.replace("{name}", &self.name);
+        base = base.replace("{repo}", &self.repo);
+        base = base.replace("{doc}", &self.documentation);
+        base = base.replace("{crate}", &self.crates_io);
+        base = base.replace("{curr}", &self.curr_vers);
+        match &self.next_vers{
+            Some(v) => {
+                base = base.replace("{next}", &v);
+            }, 
+            None => {
+                let next = "<h5>Planned Next Version: {next}</h5>";
+                base = base.replace(next, "");
+            }
+        }
+        base = base.replace("{description}", &self.description);
+        base 
     }
 }
+
+
+pub fn get_projects() -> Result<(), std::io::Error>{
+    let path = PathBuf::from("json");
+    let dir = std::fs::read_dir(path)?;
+    for entry in dir{
+        
+    }
+}
+
+
 
